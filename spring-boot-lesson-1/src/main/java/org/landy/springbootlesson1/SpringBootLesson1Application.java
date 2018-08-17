62bb9ccad9e2aeef25c08fbdc72f3aa61ccb0107
landyl <Landy.Liu@ehealth.com>
2018-08-17 17:02:23 +0800

    Spring boot初体验

--- spring-boot-lesson-1/src/main/java/org/landy/springbootlesson1/SpringBootLesson1Application.java
+++ spring-boot-lesson-1/src/main/java/org/landy/springbootlesson1/SpringBootLesson1Application.java
@@ -1,14 +1,38 @@
 package org.landy.springbootlesson1;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller //表明当前类是一个Controller
 @SpringBootApplication
-//@EnableAutoConfiguration(exclude = {org.springframework.boot.autoconfigure.gson.GsonAutoConfiguration.class})
 public class SpringBootLesson1Application {
 
 	public static void main(String[] args) {
+		//第一个参数是Sprintboot启动的来源类（source）
 		SpringApplication.run(SpringBootLesson1Application.class, args);
 	}
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
+
+	@RequestMapping("/rest")
+	@ResponseBody
+	public Map<String, Object> rest() {
+		Map<String, Object> data = new HashMap<String, Object>();
+
+		data.put("1", "A");
+		data.put("2", 2);
+
+		return data;
+	}
+
 }
