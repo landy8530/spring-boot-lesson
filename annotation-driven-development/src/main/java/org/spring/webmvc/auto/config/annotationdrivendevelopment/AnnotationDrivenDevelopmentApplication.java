39d412cdc945b0a0e4e44d7be705826f5cbb4ba0
landy8530 <landy8530@hotmail.com>
2019-01-07 22:20:56 +0800

    添加注解驱动开发案例

---
+++ annotation-driven-development/src/main/java/org/spring/webmvc/auto/config/annotationdrivendevelopment/AnnotationDrivenDevelopmentApplication.java
@@ -0,0 +1,14 @@
+package org.spring.webmvc.auto.config.annotationdrivendevelopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnotationDrivenDevelopmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnotationDrivenDevelopmentApplication.class, args);
+	}
+
+}
+
---
+++ annotation-driven-development/src/main/java/org/spring/webmvc/auto/config/annotationdrivendevelopment/controller/DemoController.java
@@ -0,0 +1,17 @@
+package org.spring.webmvc.auto.config.annotationdrivendevelopment.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Landy on 2019/1/7.
+ */
+@RestController
+public class DemoController {
+
+    @GetMapping("/helloWorld")
+    public String helloWorld(){
+        return "Hello, World!";
+    }
+
+}
